a0ba33228eab6be416cc90f7b5f954b49549c6dd
[Anurag Raut] <rautab0@gmail.com>
2024-12-26 14:28:07 +0530

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import React, { useReducer, useRef, useState, type Dispatch, type SetStateAction } from 'react';
-import { StyleSheet, Text, View, PanResponder, UIManager } from "react-native";
-import throttle from "lodash.throttle";
+import React, {  useRef, useState, type Dispatch, type SetStateAction } from 'react';
+import { StyleSheet, Text, View, PanResponder, Animated, type PanResponderGestureState } from "react-native";
 
 type Center = {
   x: number;
@@ -19,7 +18,7 @@ type Element = {
 const colors = {
   selectedColor: 'rgba(255, 99, 85, 1)',
   selectedColorBackground: "rgba(255, 99, 85, 0.1)",
-  unSelectedColorBackground: "rgba(255, 255, 255, 0.05)",
+  unSelectedColorBackground: "lightgray",
   unSelectedColor: "rgba(0, 0, 0, 1)"
 }
 
@@ -38,12 +37,13 @@ const styles = StyleSheet.create({
   },
   topComponentText: {
     fontSize: 20,
-    backgroundColor: "lightgray",
+    backgroundColor: colors.unSelectedColorBackground,
     padding: 10,
     borderRadius: 8,
+    fontWeight: "500",
 
   },
-  clock: {
+  clockContainer: {
     height: 200,
     width: 200,
     justifyContent: "center",
@@ -51,6 +51,14 @@ const styles = StyleSheet.create({
     borderRadius: 10000,
     backgroundColor: "gray",
     position: "relative",
+  },
+  clock: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10000,
+    position: "absolute",
   },
   number: {
     position: "absolute",
@@ -103,7 +111,8 @@ const styles = StyleSheet.create({
   AmPmContainerText: {
     fontSize: 12,
     paddingHorizontal: 8,
-    color: colors.unSelectedColor
+    color: colors.unSelectedColor,
+
 
   }
 });
@@ -121,22 +130,20 @@ function ElementsComponent({
   center,
   step = 1
 }: {
-  radius: number;
+  radius: number
   elements: Element[]
-  value: number;
-  setValue: Dispatch<SetStateAction<number>>;
-  center: Center,
+  value: number
+  setValue: Dispatch<SetStateAction<number>>
+  center: Center
   step: number
 }) {
 
-  console.log("ELEMTSN", elements.length
-  )
   return (
     <>
       {elements?.map(({ position }, index) => (
         <React.Fragment key={index}>
           {
-            index % step === 0 &&
+            (index % step )=== 0 &&
             <Text
 
               onPress={() => setValue(index)}
@@ -146,7 +153,7 @@ function ElementsComponent({
                   left: position.x - 15,
                   top: position.y - 15,
                 },
-                value === index && styles.selectedNumber,
+                (value === index) && styles.selectedNumber,
               ]}
             >
               {elements[index]?.value}
@@ -170,15 +177,15 @@ function ElementsComponent({
 }
 
 export default function TimePicker({ radius }: { radius: number }) {
-  const containerRef = useRef();
+  const containerRef = useRef<View | null>(null);
   const [layout, setLayout] = useState<{ width: number; height: number; screenCenter: Center; } | null>(null);
-  const hours = Array.from({ length: 12 }, (_, index) => index + 1);
+  const hours = [12, ...Array.from({ length: 11 }, (_, index) => index + 1)];
   const minutes = Array.from({ length: 60 }, (_, index) => index);
 
   const [hour, setHour] = useState<number>(0);
   const [minute, setMinute] = useState<number>(0);
   const [period, setPeriod] = useState<"am" | "pm">("am")
-  const [isHourMode, setIsHourMode] = useState(false);
+  const [isHourMode, setIsHourMode] = useState(true);
 
   const [center, setCenter] = useState<Center>({ x: 0, y: 0 });
   const [hourElements, setHourElements] = useState<Element[]>([]);
@@ -208,17 +215,17 @@ export default function TimePicker({ radius }: { radius: number }) {
       minuteElements.push({ position: { x, y }, value: minutes[i]! });
     }
     let screenX = 0, screenY = 0;
-    containerRef?.current?.measure((x, y) => {
-      console.log("PAGE", x, y)
-      screenX = x;
-      screenY = y
+    containerRef.current?.measure((_, __, ___, ____, scrX: number, scrY: number) => {
+      screenX = scrX;
+      screenY = scrY
+
     })
     setHourElements(hourElements)
     setMinuteElements(minuteElements)
     setLayout({ width, height, screenCenter: { x: screenX, y: screenY } });
   };
 
-  const updateValue = (value: number, isHourMode: number) => {
+  const updateValue = (value: number, isHourMode: boolean) => {
     if (isHourMode) {
       setHour(value);
     }
@@ -242,62 +249,102 @@ export default function TimePicker({ radius }: { radius: number }) {
     callBack(closest)
   }
 
-  const throttledUpdate = throttle((gestureState, elements: Element[]) => {
+  const throttledUpdate = (gestureState:PanResponderGestureState, elements: Element[]) => {
     const positionXScreen = gestureState.moveX - (layout?.screenCenter?.x ?? 0);
     const positionYScreen = gestureState.moveY - (layout?.screenCenter?.y ?? 0);
-    getClosetElement(positionXScreen, psotionYScreen, elements, (index) => {
-      console.log(index)
+    getClosetElement(positionXScreen, positionYScreen, elements, (index) => {
       updateValue(index, isHourMode)
     });
 
-  }, 100);
+  }
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (_, gestureState) => {
       throttledUpdate(gestureState, isHourMode ? hourElements : minuteElements);
     },
-    onPanResponderRelease: (_, gestureState) => {
 
-    },
   });
 
+  const switchAnimation = useRef(new Animated.Value(1)).current;
+
+  function switchMode(isHourMode: boolean) {
+    Animated.timing(switchAnimation, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start(() => {
+      // Callback after first animation completes
+      setIsHourMode(isHourMode)
+
+      // Start the second animation
+      Animated.timing(switchAnimation, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: false,
+      }).start();
+    });
+  }
+
+  const scaleValue = switchAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1.2, 1], 
+  });
+  
+  
+
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container}
+    >
       <View style={styles.topComponent}>
-        <Text onPress={() => setIsHourMode(true)} style={styles.topComponentText}>
-          {hour}
+        <Text onPress={() => switchMode(true)} style={[styles.topComponentText, isHourMode && { color: colors.selectedColor, backgroundColor: colors.selectedColorBackground }]}>
+          {hour.toString().padStart(2, "0")}
         </Text>
-        <Text onPress={() => setIsHourMode(false)} style={styles.topComponentText}>
-          {minute}
+        <Text onPress={() => switchMode(false)} style={[styles.topComponentText, !isHourMode && { color: colors.selectedColor, backgroundColor: colors.selectedColorBackground }]}>
+          {minute.toString().padStart(2, "0")}
         </Text>
         <View style={styles.AmPmContainer}>
-          <Text onPress={() => setPeriod("am")} style={[styles.AmPmContainerText, period === "am" && { backgroundColor: colors.selectedColorBackground,color:colors.selectedColor }]}>
+          <Text onPress={() => setPeriod("am")} style={[styles.AmPmContainerText, period === "am" && { backgroundColor: colors.selectedColorBackground, color: colors.selectedColor }]}>
             AM
           </Text>
-          <Text onPress={() => setPeriod("pm")} style={[styles.AmPmContainerText, period === "pm" &&{ backgroundColor: colors.selectedColorBackground,color:colors.selectedColor }]}>
+          <Text onPress={() => setPeriod("pm")} style={[styles.AmPmContainerText, period === "pm" && { backgroundColor: colors.selectedColorBackground, color: colors.selectedColor }]}>
             PM
           </Text>
         </View>
       </View>
       <View
-        {...panResponder.panHandlers}
-        style={[styles.clock, { height: radius * 2, width: radius * 2 }]}
-        onLayout={handleLayout}
         ref={containerRef}
+        {...panResponder.panHandlers}
+        style={[styles.clockContainer, { height: radius * 2, width: radius * 2 }]}
+        onLayout={handleLayout}
       >
-        <View style={styles.centerComponent}>
+        <View style={styles.centerComponent} />
+        <Animated.View
+          style={[styles.clock,
 
-        </View>
-        <ElementsComponent
-          radius={radius}
-          value={isHourMode ? hour : minute}
-          setValue={isHourMode ? setHour : setMinute}
-          elements={isHourMode ? hourElements : minuteElements}
-          center={center}
-          step={isHourMode ? 1 : 5}
+          {
+            opacity: switchAnimation,
+            transform: [
+              {
+                scale: scaleValue, 
+              },
+            ],
+      
+          }
+          ]}
+        >
 
-        />
+          <ElementsComponent
+            radius={radius}
+            value={isHourMode ? hour : minute}
+            setValue={isHourMode ? setHour : setMinute}
+            elements={isHourMode ? hourElements : minuteElements}
+            center={center}
+            step={isHourMode ? 1 : 5}
+
+          />
+        </Animated.View>
 
       </View>
     </View>
